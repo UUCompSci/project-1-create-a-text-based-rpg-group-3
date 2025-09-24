8c0dd661b57d6601db3287dd171f9aa46e9018c9
JoeGarretson <justjoegarr@gmail.com>
2025-09-23 22:01:11 -0500

    random hallways and rooms

--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -4,15 +4,22 @@ class Program
 {
   //================== Initialize Variables ===================
 
-  //------ scenes -------
+  //------ scenes / areas -------
   const byte INTRO = 0;
   const byte LAB = 1;
-  const byte HALLWAY = 2;
+  const byte HALLWAY = 2; //(1-20)
+  const byte HALLWAY2 = 3; //(20-40)
+  const byte CONTAINMENT = 4; // anti matter // encounter an alien // (40-50)
+  const byte NORTHSEC = 5; // anti matter (50-60)
+  const byte CONFERENCE = 6; // anti matter (60-70)
+  const byte RADIATION = 7; // 5 damage (70-80)
+  const byte NORMAL = 8; // (80-98)
+  const byte ESCAPEPOD = 9; //(99-100)
 
   static byte? currentArea = INTRO;
 
   //------ values -------
-  static int coins = 0;
+  static int antiMatter = 0;
   static int maxHP = 20;
   static int HP = maxHP;
 
@@ -31,17 +38,18 @@ class Program
   const ConsoleKey Z = ConsoleKey.Z;
   const ConsoleKey X = ConsoleKey.X;
   const ConsoleKey C = ConsoleKey.C;
-  const ConsoleKey V = ConsoleKey.V; //not used yet
+  const ConsoleKey V = ConsoleKey.V; 
   const ConsoleKey Y = ConsoleKey.Y; //not used yet (YES)
   const ConsoleKey N = ConsoleKey.N; //not used yet (NO)
   const ConsoleKey ESC = ConsoleKey.Escape;
 
   //======================== INVENTORY =========================
 
-  static string[] items = { "Keys", "Lead Pipe" };
   static void PrintInventory()
   {
-    WriteLine($"You have {coins} coins and {items[0]} and {items[1]}");
+    WriteLine($"You have {antiMatter} anti matter.");
+    WriteLine($"What is this power?");
+    WriteLine($"Your health is at {HP} out of {maxHP}.");
   }
 
   //======================== MAIN =============================
@@ -63,11 +71,32 @@ class Program
                   IntroScene();
                   break;
               case LAB:
-                  //SpawnLoop();
+                  LabScene();
                   break;
               case HALLWAY:
                   HallLoop(ref exit);
-                  //CheckDeath();
+                  break;
+              case HALLWAY2:
+                  HallLoop(ref exit);
+                  Console.WriteLine("Why are these halls so long?");
+                  break;
+              case CONTAINMENT:
+                  ContainmentScene();
+                  break;
+              case NORTHSEC:
+                  NorthScene();
+                  break;
+              case CONFERENCE:
+                  ConferenceScene();
+                  break;
+              case RADIATION:
+                  RadiationScene();
+                  break;
+              case NORMAL:
+                  NormalScene();
+                  break;
+              case ESCAPEPOD:
+                  EscapepodScene();
                   break;
               default:
                   WriteLine("You died!");
@@ -79,28 +108,23 @@ class Program
       Console.ReadKey();
   }
 
-  //======================= GAME LOOP ==========================
+  //======================= ESCAPE ATTEMPT ==========================
 
-  
+  static void EscapeAttempt()
+  {
+    Console.WriteLine("You attempt to make your escape here");
+    int escapeValue = rng.Next(11);
+    Console.WriteLine($"RNG: {escapeValue}");
+    if (escapeValue == 7)
+    {
+      Console.WriteLine("You successfully escaped");
+    }
+  }
 
   //===================== OPTIONS LOOP ==========================
 
-  
-
-  //========================= SCENES ===========================
-
-  static void IntroScene()
+  static void StarterLoop()
   {
-    Console.WriteLine("=====================================================");
-    Console.WriteLine("                   THE AWAKENING");
-    Console.WriteLine("=====================================================\n");
-
-    Console.WriteLine("You awaken in a dim laboratory. Glass shards and");
-    Console.WriteLine("cracked steel containment tubes surround you.");
-    Console.WriteLine("Your body hums with unstable energy—");
-    Console.WriteLine("you are the result of a failed experiment.");
-    Console.WriteLine("Yet somehow… you are alive.\n");
-
     Console.WriteLine("What do you do?");
     Console.WriteLine("Z. Examine your body.");
     Console.WriteLine("X. Check surroundings.");
@@ -115,7 +139,7 @@ class Program
         CheckSurroundings();
         break;
       case C:
-        //EscapeAttempt();
+        Console.WriteLine("You fail to escape");
         break;
       default:
         Console.WriteLine("\nYour unstable mind wavers, unable to decide…");
@@ -123,15 +147,103 @@ class Program
     }
   }
 
+  //========================= SCENES ===========================
+  static bool introTextShown = false;
+
+  static void IntroScene()
+  {
+    if (!introTextShown)
+    {
+      Console.WriteLine("=====================================================");
+      Console.WriteLine("                   THE AWAKENING");
+      Console.WriteLine("=====================================================\n");
+
+      Console.WriteLine("You awaken in a dim laboratory. Glass shards and");
+      Console.WriteLine("cracked steel containment tubes surround you.");
+      Console.WriteLine("Your body hums with unstable energy—");
+      Console.WriteLine("you are the result of a failed experiment.");
+      Console.WriteLine("Yet somehow… you are alive.\n");
+
+      introTextShown = true;
+    }
+
+    StarterLoop();
+  }
+
+  static void LabScene()
+  {
+    Console.WriteLine("A sign reads \"LAB\"");
+    currentArea = HALLWAY;
+  }
+
+// THE core loop of the game sort of
   static void HallLoop(ref bool exit) //Random number for which hall it is
   {
-    WriteLine("Placeholder for hallway exploration");
-    WriteLine("Press 'z' to look around, 'esc' to quit: ");
+    WriteLine("");
+    WriteLine("Z. Look around");
+    WriteLine("X. Go forward");
+    //WriteLine("C. Attempt to escape");
+    WriteLine("V. View status");
+    WriteLine("ESC: Quit game");
+
 
     switch (GetChoice())
     {
       case Z:
-        Console.WriteLine("You see a florescent glare in the hall ahead. Around you are panels on the wall with blue tubes of light");
+        CheckSurroundings();
+        break;
+      case X:
+        Console.WriteLine("You go forward through the halls");
+        int hallRoll = rng.Next(100);
+
+        if (hallRoll < 20)
+        {
+          currentArea = HALLWAY;
+        }
+
+        else if (hallRoll < 40)
+        {
+          currentArea = HALLWAY2;
+        }
+
+        else if (hallRoll < 50)
+        {
+          currentArea = CONTAINMENT;
+        }
+
+        else if (hallRoll < 60)
+        {
+          currentArea = NORTHSEC;
+        }
+
+        else if (hallRoll < 70)
+        {
+          currentArea = CONFERENCE;
+        }
+
+        else if (hallRoll < 80)
+        {
+          currentArea = RADIATION;
+        }
+
+        else if (hallRoll < 99)
+        {
+          currentArea = NORMAL;
+        }
+
+        else
+        {
+          currentArea = ESCAPEPOD;
+        }
+
+        break;
+      /*
+      case C:
+        EscapeAttempt();
+        break;
+      */
+      case V:
+        PrintInventory();
         break;
       case ESC:
         exit = true;
@@ -140,6 +252,42 @@ class Program
         WriteLine("You hesitate and catch your breath");
         break;
     }
+  }
+
+  static void ContainmentScene()
+  {
+    Console.WriteLine("A sign reads \"CONTAINMENT_CENTER_3\"");
+    currentArea = HALLWAY;
+  }
+
+  static void NorthScene()
+  {
+    Console.WriteLine("A sign reads \"NORTHERN_SECURITY\"");
+    currentArea = HALLWAY;
+  }
+
+  static void ConferenceScene()
+  {
+    Console.WriteLine("A sign reads \"CONFERENCE_4\"");
+    currentArea = HALLWAY;
+  }
+
+  static void RadiationScene()
+  {
+    Console.WriteLine("A sign reads \"CAUTION: RADIATION\"");
+    currentArea = HALLWAY;
+  }
+
+  static void NormalScene()
+  {
+    Console.WriteLine("There is nothing interesting about this room.");
+    currentArea = HALLWAY;
+  }
+
+  static void EscapepodScene()
+  {
+    Console.WriteLine("A sign reads \"ESCAPEPODS\"");
+    currentArea = HALLWAY;
   }
 
   // Scenarios
@@ -154,6 +302,7 @@ class Program
     Console.WriteLine("1. Focus your energy inward to stabilize.");
     Console.WriteLine("2. Release a burst of energy outward.");
     Console.Write("Choice: ");
+    Console.Write("");
     string input = Console.ReadLine() ?? string.Empty;
 
     if (input == "1")
@@ -162,28 +311,67 @@ class Program
       Console.WriteLine("\nA shockwave rattles the room! Glass shatters further, alarms faintly beep.");
     else
       Console.WriteLine("\nYou flicker uncertainly, doing nothing.");
+    PostExamineBody();
+  }
+  
+  static void PostExamineBody()
+  {
+      Console.WriteLine("What do you do?");
+      Console.WriteLine("Z. Check Surroundings.");
+      Console.WriteLine("X. Attempt to escape.\n");
+
+      switch (GetChoice())
+      {
+          case Z:
+              CheckSurroundings();
+              break;
+          case X:
+              //EscapeAttempt();
+              break;
+          default:
+              Console.WriteLine("\nYour unstable mind wavers, unable to decide…");
+              break;
+      }
   }
 
   static void CheckSurroundings()
   {
+
     if (currentArea == INTRO) // Search laboratory
     {
       Console.WriteLine("\nYou scan the ruined laboratory. Broken computers hum faintly.");
+      Console.WriteLine("\nThere is a mysterious hallway with an almost blinding florescent light");
       Console.WriteLine("Scrawled notes litter the floor: \"Subject unstable… sentience improbable.\"");
       Console.WriteLine("It seems the scientists never expected you to live.\n");
 
       Console.WriteLine("What now?");
       Console.WriteLine("1. Examine the notes more closely.");
       Console.WriteLine("2. Check the glowing console in the corner.");
+      Console.WriteLine("3. Walk into the hallway.");
       Console.Write("Choice: ");
+      Console.Write("");
       string input = Console.ReadLine() ?? string.Empty;
 
       if (input == "1")
-          Console.WriteLine("\nThe notes describe you as 'Element X-13'… a failed weapon prototype.");
+      {
+        Console.WriteLine("\nThe notes describe you as 'Element X-13'… a failed weapon prototype.");
+      }
       else if (input == "2")
-          Console.WriteLine("\nThe console flickers: SECURITY LOCKDOWN ENGAGED. A map of the facility is displayed. The map shows circles around CONTAINMENT_CENTER_3, NORTHERN_SECURITY, and CONFERENCE_4");
+      {
+        Console.WriteLine("\nThe console flickers: SECURITY LOCKDOWN ENGAGED. A map of the facility is displayed. The map shows circles around CONTAINMENT_CENTER_3, NORTHERN_SECURITY, and CONFERENCE_4");
+      }
+      else if (input == "3")
+      {
+        currentArea = HALLWAY;
+      }
       else
-          Console.WriteLine("\nYou hesitate, doing nothing.");
+        Console.WriteLine("\nYou hesitate, doing nothing.");
+    }
+
+    if (currentArea == HALLWAY)
+    {
+      Console.Write("You look around the hall. Metal beams line the walls with blueish florescent light immenating from the roof.");
+      Console.Write(" The walls seem to be curved inward. The hall seems round in shape.");
     }
   }
 }
--- TextRPG/obj/Debug/net9.0/TextRPG.AssemblyInfo.cs
+++ TextRPG/obj/Debug/net9.0/TextRPG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9c6f5c8114d17d0eda6b8779f3675482faaf6ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc3c99f1007e98b4c76041fcb903c9f29b7639c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
